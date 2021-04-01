f336115af85d32ad3fcf5bbafbdada7db6cd1ade
pixsil <57232302+pixsil@users.noreply.github.com>
2021-04-01 11:17:28 +0200

    Update pix-validate.js

--- pix-validate.js
+++ pix-validate.js
@@ -1,11 +1,10 @@
-// v4
+// v5
 
-import PValidateComponent from './pix-validate-component';
+import PixValidateComponent from './pix-validate-component';
 
 export default {
 
     install(Vue, options) {
-
         Vue.component('pix-validate', PixValidateComponent);
     }
 }
